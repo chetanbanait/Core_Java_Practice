cb80143fd340b6bc14549097295fa0700075d61c
CHETAN BANAIT <121280853+chetanbanait@users.noreply.github.com>
2025-06-18 13:31:18 +0530

    Update ArithmeticOperators.java

--- UNIT 2 PROGRAMS/ArithmeticOperators.java
+++ UNIT 2 PROGRAMS/ArithmeticOperators.java
@@ -5,6 +5,7 @@ class ArithmeticOperators
 		int no1=10;
 		int no2=20;
 		//int res=no1 + no2;
+		
 		System.out.println("result is : "+(no1+no2));
 		System.out.println("result is : "+no1+no2);
 	}
