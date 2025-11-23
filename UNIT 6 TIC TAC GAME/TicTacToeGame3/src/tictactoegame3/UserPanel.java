9fe784995062deda1e31bb5d252ea2d24004e00a
CHETAN BANAIT <121280853+chetanbanait@users.noreply.github.com>
2025-11-23 22:24:10 +0530

    Change author name in UserPanel.java
    Updated author information in UserPanel.java

--- UNIT 6 TIC TAC GAME/TicTacToeGame3/src/tictactoegame3/UserPanel.java
+++ UNIT 6 TIC TAC GAME/TicTacToeGame3/src/tictactoegame3/UserPanel.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package tictactoegame3;
 
 import java.awt.event.ActionEvent;
@@ -15,7 +11,7 @@ import javax.swing.JTextField;
 
 /**
  *
- * @author Deepak
+ * @author chetan
  */
 public class UserPanel implements ActionListener
 {
