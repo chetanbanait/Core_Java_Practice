711c750af866d584610ad39d7a0cc9891a6c4576
CHETAN BANAIT <121280853+chetanbanait@users.noreply.github.com>
2025-11-25 08:22:06 +0530

    Update Main2.java

--- Unit 5 AWT & Swing/SwingEventDemo/src/demo/Main2.java
+++ Unit 5 AWT & Swing/SwingEventDemo/src/demo/Main2.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package demo;
 
 import java.awt.Color;
@@ -17,7 +13,7 @@ import javax.swing.JTextField;
 
 /**
  *
- * @author Deepak
+ * @author Chetan
  */
 
 class Test2 implements ActionListener, MouseListener
